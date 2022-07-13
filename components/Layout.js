43dd0d984857c57d8a5b394e8ca4ab58b864cfb9
Theuswap <109206895+Theuswap@users.noreply.github.com>
2022-07-13 16:32:29 -0300

    .

--- components/Layout.js
+++ components/Layout.js
@@ -8,12 +8,12 @@ const Layout = ({ children }) => {
           <meta charSet="utf-8" />
           <title>Uniswap Interface</title>
           <meta name="description" content="Swap or provide liquidity on the Uniswap Protocol" />
-          <link rel="shortcut icon" type="image/png" href="./static/favicon.png" />
-          <link rel="apple-touch-icon" sizes="192x192" href="./static/images/192x192_App_Icon.png" />
-          <link rel="apple-touch-icon" sizes="512x512" href="./static/images/512x512_App_Icon.png" />
+          <link rel="shortcut icon" type="image/png" href="<%=baseURL%>/static/favicon.png" />
+          <link rel="apple-touch-icon" sizes="192x192" href="<%=baseURL%>/static/images/192x192_App_Icon.png" />
+          <link rel="apple-touch-icon" sizes="512x512" href="<%=baseURL%>/static/images/512x512_App_Icon.png" />
           <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
           <meta name="theme-color" content="#ff007a" />
-          <link rel="preload" href="./static/fonts/Inter-roman.var.woff2" as="font" type="font/woff2" crossOrigin="" />
+          <link rel="preload" href="<%=baseURL%>/static/fonts/Inter-roman.var.woff2" as="font" type="font/woff2" crossOrigin="" />
         </Head>
 
         <Navbar />
