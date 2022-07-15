fc54f6a820f1912e944bbe6f62b6369e5f304f66
Theuswap <109206895+Theuswap@users.noreply.github.com>
2022-07-15 19:03:43 -0300

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import '../public/styles.css';
+import TagManager from 'react-gtm-module';
 import { AppProvider } from '../context/AppContext';
 import { MoralisProvider } from "react-moralis";
+import { useEffect } from 'react';
 const APP_ID = "tmaPMGyz9Mksp6zcdqsG5sLiKUWdYupJAxSkG5q1";
 const SERVER_URL = "https://pgl6znodo88m.usemoralis.com:2053/server";
 
 function MyApp({ Component, pageProps }) {
+
+    useEffect(() => {
+        TagManager.initialize({ gtmId: 'GTM-TMM7VT8' });
+    }, []);
+
+
     return (
         <MoralisProvider 
             appId={APP_ID}
